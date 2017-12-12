9aec003dd5084ace32580e879c2ea23fc2bf8fc4
robindecroon <robindecroon@msn.com>
2017-12-13 00:06:34 +0100

    First version of Daystreak

--- client/main.js
+++ client/main.js
@@ -2,12 +2,20 @@ import React from 'react';
 import i18n from 'meteor/universe:i18n';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
+
 import TimeLeft from '../imports/ui/TimeLeft.js';
 import WeekOverview from '../imports/ui/WeekOverview.js';
+import DaysStreak from '../imports/ui/DaysStreak.js';
 
- 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
+
 Meteor.startup(() => {
-  i18n.setLocale('en')  
-  render(<WeekOverview />, document.getElementById('render-target'));
+  i18n.setLocale('en')
+  render((
+    <MuiThemeProvider>
+      <DaysStreak />
+    </MuiThemeProvider>)
+    , document.getElementById('render-target'));
 });
---
+++ imports/ui/DaysStreak.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import i18n from 'meteor/universe:i18n';
+import CircularProgressbar from 'react-circular-progressbar';
+
+const T = i18n.createComponent();
+
+export default class DaysStreak extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            completed: 100,
+        };
+    }
+
+    render() {
+        // TODO: change to actual nbDays (this.props.dayStreak * 100), pct /100
+        return (
+            <div className="panacea-text">
+                <div>
+                    <CircularProgressbar className="CircularProgressbar" percentage={60} textForPercentage={(pct) => `${pct / 20}`} />
+                    <CircularProgressbar className="CircularProgressbar-streak" percentage={500} textForPercentage={(pct) => `${pct / 100}`} />
+                </div>
+                <div className="full-width">
+                    <div className="half-element smalltext">
+                        <T>nb_measures_to_perform</T>
+                    </div>
+                    <div className="half-element smalltext">
+                        <T>avoid_lose_daystreak</T>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
