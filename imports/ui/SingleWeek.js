dcc24764f87ca2dfa57d6346fc5bf97ed1d4888a
robindecroon <robindecroon@msn.com>
2017-12-12 18:00:53 +0100

    some code styling

--- imports/ui/SingleWeek.js
+++ imports/ui/SingleWeek.js
@@ -14,27 +14,13 @@ export default class SingleWeek extends Component {
         <div className="week-label">
           <T>week</T>{" " + (this.props.weeknb + 1)}
         </div>
-        <SingleDay
-          day={(this.props.weeknb + 1) * 1} 
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 2}
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 3}
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 4}
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 5}
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 6}
-        />
-        <SingleDay
-          day={(this.props.weeknb + 1) * 7}
-        />
+        <SingleDay day={(this.props.weeknb + 1) * 1} />
+        <SingleDay day={(this.props.weeknb + 1) * 2} />
+        <SingleDay day={(this.props.weeknb + 1) * 3} />
+        <SingleDay day={(this.props.weeknb + 1) * 4} />
+        <SingleDay day={(this.props.weeknb + 1) * 5} />
+        <SingleDay day={(this.props.weeknb + 1) * 6} />
+        <SingleDay day={(this.props.weeknb + 1) * 7} />
       </li>
     );
   }
