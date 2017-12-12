4796db5f6776c5f2f15ef996944a06f86fc56f14
robindecroon <robindecroon@msn.com>
2017-12-12 14:27:27 +0100

    added plural localization

--- imports/ui/TimeLeft.js
+++ imports/ui/TimeLeft.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 import countdown from 'moment-countdown';
 
 const T = i18n.createComponent();
-i18n.setLocale('nl')
 
 // App component - represents the whole app
 export default class TimeLeft extends Component {
@@ -18,6 +17,9 @@ export default class TimeLeft extends Component {
             timeHour: ('0' + end.diff(now, 'hours')).slice(-2),
             timeMinute: ('0' + end.diff(now, 'minutes') % 60).slice(-2),
             timeSecond: ('0' + end.diff(now, 'seconds') % 60).slice(-2),
+            labelHours: i18n.__("hours"),
+            labelMinutes: i18n.__("minutes"),
+            labelSeconds: i18n.__("seconds"),
         };
 
         // start countdown timer
@@ -28,12 +30,19 @@ export default class TimeLeft extends Component {
 
     updateTimer(end) {
         const now = moment();
+        const hours = end.diff(now, 'hours');
+        const minutes = end.diff(now, 'minutes') % 60;
+        const seconds = end.diff(now, 'seconds') % 60;
+
         this.setState({
             // code copied to constructor (not possible to update state of unmounted component)
-            timeHour: ('0' + end.diff(now, 'hours')).slice(-2),
-            timeMinute: ('0' + end.diff(now, 'minutes') % 60).slice(-2),
-            timeSecond: ('0' + end.diff(now, 'seconds') % 60).slice(-2),
-        })
+            timeHour: ('0' + hours).slice(-2),
+            timeMinute: ('0' + minutes).slice(-2),
+            timeSecond: ('0' + seconds).slice(-2),
+            labelHours: hours > 1 ? i18n.__("hours") : i18n.__("hour"),
+            labelMinutes: minutes > 1 ? i18n.__("minutes") : i18n.__("minute"),
+            labelSeconds: seconds > 1 ? i18n.__("seconds") : i18n.__("second"),
+        });
     }
 
     render() {
@@ -45,15 +54,15 @@ export default class TimeLeft extends Component {
                 <div id="clockdiv">
                     <div>
                         <span className="timeUnit">{this.state.timeHour}</span>
-                        <div className="smalltext"><T>hours</T></div>
+                        <div className="smalltext">{this.state.labelHours}</div>
                     </div>
                     <div>
                         <span className="timeUnit">{this.state.timeMinute}</span>
-                        <div className="smalltext"><T>minutes</T></div>
+                        <div className="smalltext">{this.state.labelMinutes}</div>
                     </div>
                     <div>
                         <span className="timeUnit">{this.state.timeSecond}</span>
-                        <div className="smalltext"><T>seconds</T></div>
+                        <div className="smalltext">{this.state.labelSeconds}</div>
                     </div>
                 </div>
             </div>
