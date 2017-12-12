dfb93618633e44cc1e9a6a462e746ebec5b33a2d
robindecroon <robindecroon@msn.com>
2017-12-12 13:57:07 +0100

    succesful integration of i18n:universe

--- client/main.js
+++ client/main.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import i18n from 'meteor/universe:i18n';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
 import TimeLeft from '../imports/ui/TimeLeft.js';
  
 Meteor.startup(() => {
+  i18n.setLocale('en')  
   render(<TimeLeft />, document.getElementById('render-target'));
 });
--- imports/ui/TimeLeft.js
+++ imports/ui/TimeLeft.js
@@ -1,51 +1,59 @@
 import React, { Component } from 'react';
+import i18n from 'meteor/universe:i18n';
 import moment from 'moment';
 import countdown from 'moment-countdown';
 
+const T = i18n.createComponent();
+i18n.setLocale('nl')
 
 // App component - represents the whole app
 export default class TimeLeft extends Component {
     constructor(props) {
         super(props);
 
-        const end = moment().endOf('day');
-     
+        const end = moment().endOf('day');         
+        const now = moment();   
         this.state = {
-            timeHour: 0,
-            timeMinute: 0,
-            timeSecond: 0,
+            // code copied from updateTime (not possible to update state of unmounted component)
+            timeHour: ('0' + end.diff(now, 'hours')).slice(-2),
+            timeMinute: ('0' + end.diff(now, 'minutes') % 60).slice(-2),
+            timeSecond: ('0' + end.diff(now, 'seconds') % 60).slice(-2),
         };
 
-        let i = 0;
+        // start countdown timer
         setInterval(() => {
-            const now = moment();
-            
-            this.setState({
-                timeHour: ('0' + end.diff(now, 'hours')).slice(-2),
-                timeMinute: ('0' + end.diff(now, 'minutes') % 60).slice(-2),
-                timeSecond: ('0' + end.diff(now, 'seconds') % 60).slice(-2),
-            })
+            this.updateTimer(end);
         }, 1000);
-      }
+    }
+
+    updateTimer(end) {
+        const now = moment();
+        this.setState({
+            // code copied to constructor (not possible to update state of unmounted component)
+            timeHour: ('0' + end.diff(now, 'hours')).slice(-2),
+            timeMinute: ('0' + end.diff(now, 'minutes') % 60).slice(-2),
+            timeSecond: ('0' + end.diff(now, 'seconds') % 60).slice(-2),
+        })
+    }
 
     render() {
         return (
             <div className="panacea-text">
                 <h1>
-                    Limited time left to register your measurements for today!
+                    <T>header_time_left</T>
                 </h1>
                 <div id="clockdiv">
                     <div>
-                        <span className="hours">{this.state.timeHour}</span>
-                        <div className="smalltext">Hours</div>
-                    </div>
-                    <div> 
-                        <span className="minutes">{this.state.timeMinute}</span>
-                        <div className="smalltext">Minutes</div>
+                        <span className="timeUnit">{this.state.timeHour}</span>
+                        <div className="smalltext"><T>hours</T></div>
                     </div>
                     <div>
-                        <span className="seconds">{this.state.timeSecond}</span>
-                        <div className="smalltext">Seconds</div>
+                        <span className="timeUnit">{this.state.timeMinute}</span>
+                        <div className="smalltext"><T>minutes</T></div>
+                    </div>
+                    <div>
+                        <span className="timeUnit">{this.state.timeSecond}</span>
+                        <div className="smalltext"><T>seconds</T></div>
                     </div>
                 </div>
             </div>
