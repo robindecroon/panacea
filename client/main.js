90da9ca622958db466224d7e680cd0942386e7d0
robindecroon <robindecroon@msn.com>
2017-12-14 12:00:13 +0100

    first version of doctorfeedback

--- client/main.js
+++ client/main.js
@@ -7,15 +7,14 @@ import TimeLeft from '../imports/ui/TimeLeft.js';
 import WeekOverview from '../imports/ui/WeekOverview.js';
 import DaysStreak from '../imports/ui/DaysStreak.js';
 import NewsMain from '../imports/ui/NewsMain.js';
+import DoctorFeedbackMain from '../imports/ui/DoctorFeedbackMain.js';
 
-
+// wrap in this element to use material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-
-
 Meteor.startup(() => {
-  i18n.setLocale('en')
+  i18n.setLocale('nl')
   render(
-      <NewsMain />
+      <DoctorFeedbackMain />
     , document.getElementById('render-target'));
 });
---
+++ imports/ui/DoctorFeedbackMain.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import i18n from 'meteor/universe:i18n';
+import CircularProgressbar from 'react-circular-progressbar';
+
+const T = i18n.createComponent();
+
+export default class DoctorFeedbackMain extends Component {
+    constructor(props) {
+        super(props);
+
+        console.log(i18n.getLocale());
+        
+        // TODO: hack. Locale not update in init phase. Use i18n-key
+        this.state = {
+            feedback: "positive_feedback",
+        };
+
+    }
+
+    render() {
+        return (
+            <div className="panacea-text">
+                <div className="md66">
+                    <div className="panacea-image-text" >
+                        <div className="child"><T>{this.state.feedback}</T></div>
+                        
+                    </div>
+                </div>
+                <div className="md33">
+                    <img className="panacea-image" src="http://icons.iconarchive.com/icons/icons-land/medical/128/People-Doctor-Female-icon.png" />
+                </div>
+            </div>
+        );
+    }
+}
