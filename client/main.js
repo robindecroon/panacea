398071e3a89ca96a4a34dc4da9c820441d0f3868
robindecroon <robindecroon@msn.com>
2017-12-14 13:33:35 +0100

    First version of LevelMain

--- client/main.js
+++ client/main.js
@@ -8,6 +8,7 @@ import WeekOverview from '../imports/ui/WeekOverview.js';
 import DaysStreak from '../imports/ui/DaysStreak.js';
 import NewsMain from '../imports/ui/NewsMain.js';
 import DoctorFeedbackMain from '../imports/ui/DoctorFeedbackMain.js';
+import LevelMain from '../imports/ui/LevelMain.js';
 
 // wrap in this element to use material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -15,6 +16,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 Meteor.startup(() => {
   i18n.setLocale('nl')
   render(
-      <DoctorFeedbackMain />
+      <LevelMain />
     , document.getElementById('render-target'));
 });
---
+++ imports/ui/LevelMain.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import i18n from 'meteor/universe:i18n';
+import CircularProgressbar from 'react-circular-progressbar';
+
+const T = i18n.createComponent();
+const xpPerLevel = 600;
+
+export default class LevelMain extends Component {
+    constructor(props) {
+        super(props);
+
+        const maxLevel = 2400;
+        const xp = Math.floor(Math.random() * (maxLevel+1));
+
+        const level1Image = "/images/level1.png";
+        const level2Image = "/images/level2.png";
+        const level3Image = "/images/level3.png";
+        
+        const levelImages = [level1Image, level2Image, level3Image];
+
+        // TODO: hack. Locale not update in init phase. Use i18n-key
+        this.state = {
+            message: "train_bepett",
+            currentLevel: this.getLevel(xp),
+            imageSrc: levelImages[(this.getLevel(xp) - 1)],
+            currentXP: xp % xpPerLevel,
+        };
+    }
+
+    getLevel(xp) {
+        return Math.floor(xp/xpPerLevel) + 1;
+    }
+
+    render() {
+        const firstWidth = 100 * this.state.currentXP/xpPerLevel;
+        return (
+            <div className="panacea-text">
+                <div className="full-width">
+                    <div className="md25">
+                        <img className="panacea-image" src={this.state.imageSrc} />
+                    </div>
+                    <div className="md75">
+                        <div className="panacea-image-text" >
+                            <div><T>{this.state.message}</T></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="md66">
+                    <div className="small-text">
+                        <div><T>level</T>{" " + this.state.currentLevel}</div>
+                    </div>
+                    <div className="week-5  rh50" style={{ width: firstWidth + "%" }}></div>
+                    <div className="week-2  rh50" style={{ width: (100-firstWidth) + "%" }}></div>
+                    <div className="small-text">
+                        <div>{this.state.currentXP}/{xpPerLevel}</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
